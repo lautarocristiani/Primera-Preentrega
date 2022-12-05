c3289f9c229b0c5c0cae40af204c9fa90021f5cb
lautarocristiani <lautarocristiani2001@gmail.com>
2022-12-05 19:23:29 -0300

    sin terminar

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const fs = require("fs");
+
 class ProductManager{
-
-
 
     constructor(products){
         this.products = products;
+        this.path = "./productos.txt";
+    }
+
+    guardar(){
+        fs.writeFileSync(this.path, JSON.stringify(this.products));
+    }
+
+    leer(){
+        JSON.parse(fs.readFileSync(this.path, "utf-8"));
     }
 
     getProducts() {
@@ -15,24 +24,16 @@ class ProductManager{
             console.error("Se ha producido un error");
         } else {
             let id;
-            this.products.lenght == undefined ? id = 1 : id = this.products.lenght - 1;
-            console.log(id);
+            this.products.lenght == undefined ? id = 1 : id = this.products[this.products.length-1].id + 1;
             this.products.push({title, description, price, thumbail, code, stock, id});
         }
     }
 
     getProductById(id){
-        let encontrado = true;
-        this.products.forEach(p => {
-            if (p.id == id) {
-                console.log(p);
-                encontrado = false;
-            }
-        });
-        if (encontrado) {
-            console.error("Not found");
-        }
+        this.products.find((p) => p.id == id) ? console.log(this.products.find((p) => p.id == id)) : console.log("No se ha encontrado el producto");
     }
+
+    //agregar updateProduct y deleteProduct
 }
 
 let instancia = new ProductManager([]);
@@ -41,4 +42,4 @@ instancia.getProducts();
 instancia.addProducts("producto prueba", "Este es el producto prueba", 200, "Sin imagen", "abc123", 25);
 instancia.getProducts();
 instancia.addProducts("producto prueba", "Este es el producto prueba", 200, "Sin imagen", "abc123", 25);
-instancia.getProductById(3);
+instancia.getProductById(2);
